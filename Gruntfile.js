20a019598756a9639c5b7ef43154687eed2d898d
Jonathan Olson <olsonsjc@gmail.com>
2013-02-22 15:51:09 -0800

    QUnit, unit tests, linting in grunt, etc.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,35 +56,52 @@ module.exports = function( grunt ) {
     },
     
     jshint: {
+      all: [
+        'Gruntfile.js', 'js/**/*.js'
+      ],
+      // adjust with options from http://www.jshint.com/docs/
       options: {
-        curly: true,
+        // enforcing options
+        curly: true, // brackets for conditionals
         eqeqeq: true,
-        immed: false,
-        latedef: false,
+        immed: true,
+        latedef: true,
         newcap: true,
         noarg: true,
-        sub: true,
+        // noempty: true,
+        nonew: true,
+        // quotmark: 'single',
         undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        node: true,
-        jQuery: true,
-        expr: true
+        // unused: true, // certain layer APIs not used in cases
+        // strict: true,
+        trailing: true,
+        
+        // relaxing options
+        es5: true, // we use ES5 getters and setters for now
+        loopfunc: true, // we know how not to shoot ourselves in the foot, and this is useful for _.each
+        
+        globals: {
+          // for require.js
+          define: true,
+          require: true,
+          
+          Uint16Array: false,
+          Uint32Array: false,
+          document: false,
+          window: false,
+          console: false,
+          Float32Array: true // we actually polyfill this, so allow it to be set
+        }
       },
-      globals: {
-        Modernizr: true,
-        define: true,
-        $: true
-      }
     }
   } );
   
   // Default task.
-  grunt.registerTask( 'default', [ 'production', 'standalone' ] );
+  grunt.registerTask( 'default', [ 'jshint', 'production', 'standalone' ] );
   grunt.registerTask( 'production', [ 'requirejs:production' ] );
   grunt.registerTask( 'standalone', [ 'requirejs:standalone', 'concat:standalone', 'uglify:standalone' ] );
   grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
   grunt.loadNpmTasks( 'grunt-contrib-concat' );
   grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+  grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 };
---
+++ test/unit-tests.js
@@ -0,0 +1,8 @@
+
+(function() {
+  module( 'Dot' );
+  
+  test( 'Creation checks', function() {
+    ok( new dot.Vector2( 1, 2 ) );
+  } );
+})();
