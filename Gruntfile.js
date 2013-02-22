11114155c70f8f715be88758367d9a213decfb9f
Jonathan Olson <olsonsjc@gmail.com>
2013-02-22 15:11:16 -0800

    Better production builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,49 +13,37 @@ module.exports = function( grunt ) {
     },
     
     concat: {
-      development: {
-        "dist/debug/dot.js": [
-          "contrib/almond/almond.js",
-          "dist/debug/dot.js"
-        ]
-      },
-      
-      production: {
-        almond: true,
-        
+      standalone: {
+        src: [ "contrib/almond/almond.js", "dist/standalone/dot.min.js" ],
+        dest: "dist/standalone/dot.min.js"
       }
     },
     
     uglify: {
-      development: {
-        options: {
-          // source map
-          sourceMap: 'dist/release/dot.min.js.map'
-        },
+      standalone: {
         build: {
-          src: 'dist/debug/dot.js',
-          dest: 'dist/release/dot.min.js'
+          src: [ 'dist/standalone/dot.min.js' ],
+          dest: 'dist/standalone/dot.min.js'
         }
       }
     },
     
     requirejs: {
-      development: {
+      standalone: {
         options: {
           mainConfigFile: "js/config.js",
-          out: "dist/debug/dot.js",
+          out: "dist/standalone/dot.min.js",
           name: "config",
+          optimize: 'uglify2',
           wrap: {
             start: "(function() {",
             end: " window.dot = require( 'main' ); }());"
-          },
-          optimize: 'none'
+          }
         }
       },
       production: {
         options: {
           mainConfigFile: "js/config.js",
-          almond: true,
           out: "dist/production/dot.min.js",
           name: "config",
           optimize: 'uglify2',
@@ -95,9 +83,9 @@ module.exports = function( grunt ) {
   } );
   
   // Default task.
-  grunt.registerTask( 'default', [ 'production' ] );
+  grunt.registerTask( 'default', [ 'production', 'standalone' ] );
   grunt.registerTask( 'production', [ 'requirejs:production' ] );
-  grunt.registerTask( 'development', [ 'requirejs:development', 'concat:development', 'uglify:development' ] );
+  grunt.registerTask( 'standalone', [ 'requirejs:standalone', 'concat:standalone', 'uglify:standalone' ] );
   grunt.loadNpmTasks( 'grunt-contrib-requirejs' );
   grunt.loadNpmTasks( 'grunt-contrib-concat' );
   grunt.loadNpmTasks( 'grunt-contrib-uglify' );
