19ca01507927157b3bc7dc32e0c02d2e46e99b8e
Jonathan Olson <olsonsjc@gmail.com>
2013-02-28 14:31:53 -0800

    Adding async flag

--- tests/qunit/unit-tests.js
+++ tests/qunit/unit-tests.js
@@ -4,6 +4,7 @@ function runDotTests( pathToTestBase ) {
   function loadTestFile( src ) {
     var script = document.createElement( 'script' );
     script.type = 'text/javascript';
+    script.async = false;
     
     // make sure things aren't cached, just in case
     script.src = pathToTestBase + '/' + src + '?random=' + Math.random().toFixed( 10 );
