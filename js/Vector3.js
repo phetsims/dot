67acfee170883277314b35a6ef3fc98a2c480e3d
Jonathan Olson <olsonsjc@gmail.com>
2014-10-13 18:39:07 -0600

    Efficiency/GC improvements for Vector3.distance/distanceSquared

--- js/Vector3.js
+++ js/Vector3.js
@@ -40,12 +40,15 @@ define( function( require ) {
 
     // the distance between this vector (treated as a point) and another point
     distance: function( point ) {
-      return this.minus( point ).magnitude();
+      return Math.sqrt( this.distanceSquared( point ) );
     },
 
     // the squared distance between this vector (treated as a point) and another point
     distanceSquared: function( point ) {
-      return this.minus( point ).magnitudeSquared();
+      var dx = this.x - point.x;
+      var dy = this.y - point.y;
+      var dz = this.z - point.z;
+      return dx * dx + dy * dy + dz * dz;
     },
 
     dot: function( v ) {
