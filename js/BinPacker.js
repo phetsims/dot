f6d89faaad8670c037933a19b0ec2273cdaba9a1
Chris Malley <cmalley@pixelzoom.com>
2015-04-09 11:16:24 -0600

    remove extra space from copyright comment on line 1 of .js files

--- js/BinPacker.js
+++ js/BinPacker.js
@@ -1,4 +1,4 @@
-//  Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2002-2014, University of Colorado Boulder
 
 /**
  * Given a rectangular containing area, takes care of allocating and deallocating smaller rectangular "bins" that fit
