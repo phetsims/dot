2cd2dc4085beeafcc67933b4494fc361d0c07c7a
Sam Reid <reids@colorado.edu>
2015-05-01 20:02:04 -0600

    Removed arch assignment workaround

--- js/dot.js
+++ js/dot.js
@@ -6,9 +6,6 @@ define( function( require ) {
   // object allocation tracking
   window.phetAllocation = require( 'PHET_CORE/phetAllocation' );
 
-  // workaround for Axon, since it needs window.arch to be defined
-  window.arch = window.arch || null;
-
   var dot = function dot() {
     switch( arguments.length ) {
       case 2:
