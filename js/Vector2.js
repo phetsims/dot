b9e3c1e07271b8b4fee91fca360f51563fe479cb
samreid <reids@colorado.edu>
2015-10-28 13:52:05 -0600

    Restored Vector2.toStateObject, see https://github.com/phetsims/together/issues/107

--- js/Vector2.js
+++ js/Vector2.js
@@ -301,6 +301,11 @@ define( function( require ) {
 
     setPolar: function( magnitude, angle ) {
       return this.setXY( magnitude * Math.cos( angle ), magnitude * Math.sin( angle ) );
+    },
+
+    // support for serialization
+    toStateObject: function() {
+      return { x: this.x, y: this.y };
     }
   };
 
