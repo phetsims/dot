afc1f04c3fb2ee6cf281f340d1c553f4c7cffd51
samreid <reids@colorado.edu>
2016-09-28 20:05:38 -0600

    Adapted for chipper grunt build-js, see https://github.com/phetsims/dot/issues/64

---
+++ js/dot-main.js
@@ -0,0 +1,16 @@
+// Copyright 2016, University of Colorado Boulder
+(function() {
+  'use strict';
+  if ( !window.hasOwnProperty( '_' ) ) {
+    throw new Error( 'Underscore/Lodash not found: _' );
+  }
+  if ( !window.hasOwnProperty( '$' ) ) {
+    throw new Error( 'jQuery not found: $' );
+  }
+  define( function( require ) {
+
+    window.dot = require( 'DOT/main' );
+    window.axon = require( 'AXON/main' );
+    window.phetCore = require( 'PHET_CORE/main' );
+  } );
+})();
