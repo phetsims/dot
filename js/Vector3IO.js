c9359e7a7095bd7f9957faa8cbd3d74a79c15707
samreid <reids@colorado.edu>
2018-06-08 12:51:42 -0600

    Replaced '// phet-io modules' with '// iphetio', see https://github.com/phetsims/phet-io/issues/1331

--- js/Vector3IO.js
+++ js/Vector3IO.js
@@ -12,7 +12,7 @@ define( function( require ) {
   // modules
   var dot = require( 'DOT/dot' );
 
-  // phet-io modules
+  // ifphetio
   var assertInstanceOf = require( 'ifphetio!PHET_IO/assertInstanceOf' );
   var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
   var ObjectIO = require( 'ifphetio!PHET_IO/types/ObjectIO' );
