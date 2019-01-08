32f10f836818cc4051111365716dd9f585e7b867
chrisklus <christopher.klusendorf@colorado.edu>
2019-01-08 10:03:41 -0700

    Factor out rangeWithValue, see https://github.com/phetsims/dot/issues/77

--- js/RangeWithValueTests.js
+++ js/RangeWithValueTests.js
@@ -28,34 +28,34 @@ define( require => {
   QUnit.test( 'methods', assert => {
 
     // test a valid and invalid setMin()
-    const rangeWithValue1 = new RangeWithValue( 1, 10, 3 );
-    rangeWithValue1.setMin( 2 );
-    assert.equal( rangeWithValue1.min, 2, 'setMin() succeeds when min < defaultValue < max' );
-    window.assert && assert.throws( () => { rangeWithValue1.setMin( 4 ); }, 'setMin() fails when defaultValue < min < max' );
+    let rangeWithValue = new RangeWithValue( 1, 10, 3 );
+    rangeWithValue.setMin( 2 );
+    assert.equal( rangeWithValue.min, 2, 'setMin() succeeds when min < defaultValue < max' );
+    window.assert && assert.throws( () => { rangeWithValue.setMin( 4 ); }, 'setMin() fails when defaultValue < min < max' );
 
     // test a valid and invalid setMax()
-    const rangeWithValue2 = new RangeWithValue( 1, 10, 8 );
-    rangeWithValue2.setMax( 9 );
-    assert.equal( rangeWithValue2.max, 9, 'setMax() succeeds when max > defaultValue > min' );
-    window.assert && assert.throws( () => { rangeWithValue2.setMax( 7 ); }, 'setMax() fails when defaultValue > max > min' );
+    rangeWithValue = new RangeWithValue( 1, 10, 8 );
+    rangeWithValue.setMax( 9 );
+    assert.equal( rangeWithValue.max, 9, 'setMax() succeeds when max > defaultValue > min' );
+    window.assert && assert.throws( () => { rangeWithValue.setMax( 7 ); }, 'setMax() fails when defaultValue > max > min' );
 
     // test a true and false equals()
-    const rangeWithValue3 = new RangeWithValue( 1, 10, 5 );
+    rangeWithValue = new RangeWithValue( 1, 10, 5 );
     assert.ok(
-      rangeWithValue3.equals( new RangeWithValue( 1, 10, 5 ) ),
+      rangeWithValue.equals( new RangeWithValue( 1, 10, 5 ) ),
       'equals() succeeds when rangeWithValue1 === rangeWithValue2'
     );
     assert.notOk(
-      rangeWithValue3.equals( new RangeWithValue( 1, 10, 6 ) ),
+      rangeWithValue.equals( new RangeWithValue( 1, 10, 6 ) ),
       'equals() fails when rangeWithValue1 !== rangeWithValue2'
     );
 
     // test a valid and invalid setMinMax()
-    const rangeWithValue5 = new RangeWithValue( 1, 10, 5 );
-    rangeWithValue5.setMinMax( 2, 9 );
-    assert.ok( rangeWithValue5.equals( new RangeWithValue( 2, 9, 5 ) ), 'setMinMax() succeeds when min < defaultValue < max' );
+    rangeWithValue = new RangeWithValue( 1, 10, 5 );
+    rangeWithValue.setMinMax( 2, 9 );
+    assert.ok( rangeWithValue.equals( new RangeWithValue( 2, 9, 5 ) ), 'setMinMax() succeeds when min < defaultValue < max' );
     window.assert && assert.throws( () => {
-        rangeWithValue5.setMinMax( 3, 4 );
+      rangeWithValue.setMinMax( 3, 4 );
       }, 'setMinMax() fails when default value is out of range'
     );
   } );
