530b2baa832f1a39df1e0684be0b7bbaa769cebe
zepumph <michael.kauzmann@colorado.edu>
2019-02-11 15:27:49 -0900

    added boxplot functions to Stats, https://github.com/phetsims/dot/issues/86

--- js/Stats.js
+++ js/Stats.js
@@ -14,6 +14,36 @@ define( require => {
   const Stats = {
 
     /**
+     * Inspired by https://stackoverflow.com/questions/30893443/highcharts-boxplots-how-to-get-five-point-summary
+     * @param {Array.<number>} numbers
+     * @param {number} percentile
+     * @returns {number|null}
+     */
+    getPercentile( numbers, percentile ) {
+      assert && assert( Array.isArray( numbers ) );
+      assert && numbers.forEach( n => assert && assert( typeof n === 'number' ) );
+
+      if ( numbers.length === 0 ) {
+        return null;
+      }
+
+      numbers.sort( ( a, b ) => a - b );
+      const index = ( percentile / 100 ) * numbers.length;
+      let result = null;
+
+      // for integers
+      if ( Math.floor( index ) === index ) {
+        result = ( numbers[ ( index - 1 ) ] + numbers[ index ] ) / 2;
+      }
+      else {
+
+        // for decimal
+        result = numbers[ Math.floor( index ) ];
+      }
+      return result;
+    },
+
+    /**
      * Get the median from an unsorted array of numbers
      * @public
      *
@@ -21,25 +51,47 @@ define( require => {
      * @returns {number|null} - null if array is empty
      */
     median( numbers ) {
+      return Stats.getPercentile( numbers, 50 );
+    },
+
+    /**
+     * see https://www.whatissixsigma.net/box-plot-diagram-to-identify-outliers/ for formulas
+     * @param {Array.<number>} numbers
+     * @returns {Object} -
+     *                    q1: {number} -     first quartile
+     *                    q3: {number} -     third quartile
+     *                    median: {number} - median
+     */
+    getBoxPlotValues( numbers ) {
+      assert && assert( numbers.length >= 4, 'need at least 4 values to calculate quartiles' );
+      return {
+        q1: Stats.getPercentile( numbers, 25 ),
+        median: Stats.getPercentile( numbers, 50 ),
+        q3: Stats.getPercentile( numbers, 75 )
+      };
+    },
+
+    /**
+     * Get the limits for a data set
+     * @param {Array.<number>} numbers
+     * @returns {Object}
+     */
+    getBoxPlotLimits( numbers ) {
       assert && assert( Array.isArray( numbers ) );
+      assert && assert( numbers.length >= 4, 'need at least 4 values to calculate data limits' );
       assert && numbers.forEach( n => assert && assert( typeof n === 'number' ) );
 
-      numbers.sort( ( a, b ) => a - b );
+      const quartiles = Stats.getBoxPlotValues( numbers );
 
-      if ( numbers.length === 0 ) {
-        return null;
-      }
+      // calculate inter-quartile range
+      const iqr = quartiles.q3 - quartiles.q1;
 
-      const half = Math.floor( numbers.length / 2 );
-
-      if ( numbers.length % 2 ) {
-        return numbers[ half ];
-      }
-      else {
-
-        // if there are an even number of entries, take the median of the center 2
-        return ( numbers[ half - 1 ] + numbers[ half ] ) / 2;
-      }
+      const lowerLimit = quartiles.q1 - 1.5 * iqr;
+      const upperLimit = quartiles.q3 + 1.5 * iqr;
+      return {
+        lowerLimit: lowerLimit,
+        upperLimit: upperLimit
+      };
     }
   };
 
--- js/StatsTests.js
+++ js/StatsTests.js
@@ -39,4 +39,67 @@ define( require => {
     assert.equal( Stats.median( [] ), null );
   } );
 
+  QUnit.test( 'Quartiles', assert => {
+
+    if ( window.assert ) {
+      assert.throws( () => {
+        Stats.getBoxPlotValues( [ 'test' ] );
+      } );
+      assert.throws( () => {
+        Stats.getBoxPlotValues( 'hi' );
+      } );
+      assert.throws( () => {
+        Stats.getBoxPlotValues( [ 45, 'test' ] );
+      } );
+
+      assert.throws( () => {
+        Stats.getBoxPlotValues( [ 45 ] );
+      } );
+
+      assert.throws( () => {
+        Stats.getBoxPlotValues( [ 45, 3 ] );
+      } );
+
+      assert.throws( () => {
+        Stats.getBoxPlotValues( [ 45, 2, 2 ] );
+      } );
+    }
+
+    let limits = Stats.getBoxPlotLimits( [ 10, 20, 30, 40 ] );
+    let values = Stats.getBoxPlotValues( [ 10, 20, 30, 40 ] );
+    assert.equal( values.q1, 15 );
+    assert.equal( values.q3, 35 );
+    assert.equal( limits.lowerLimit, -15 );
+    assert.equal( limits.upperLimit, 65 );
+
+    limits = Stats.getBoxPlotLimits( [ 5, 4, 3, 1, 50 ] );
+    values = Stats.getBoxPlotValues( [ 5, 4, 3, 1, 50 ] );
+    assert.equal( values.q1, 3 );
+    assert.equal( values.q3, 5 );
+    assert.equal( limits.lowerLimit, 0 );
+    assert.equal( limits.upperLimit, 8 );
+
+
+    limits = Stats.getBoxPlotLimits( [ 5, 4, 3, 23, 1, 50 ] );
+    values = Stats.getBoxPlotValues( [ 5, 4, 3, 23, 1, 50 ] );
+    assert.equal( values.q1, 3 );
+    assert.equal( values.q3, 23 );
+    assert.equal( limits.lowerLimit, -27 );
+    assert.equal( limits.upperLimit, 53 );
+
+    limits = Stats.getBoxPlotLimits( [ 35, 36, 46, 53, 58, 65, 75, 94 ] );
+    values = Stats.getBoxPlotValues( [ 35, 36, 46, 53, 58, 65, 75, 94 ] );
+    assert.equal( values.q1, 41 );
+    assert.equal( values.q3, 70 );
+    assert.equal( limits.lowerLimit, -2.5 );
+    assert.equal( limits.upperLimit, 113.5 );
+
+    limits = Stats.getBoxPlotLimits( [ 21, 25, 24, 23, 22, 22, 200, 24, 28, 29, 20, 29, 28 ] );
+    values = Stats.getBoxPlotValues( [ 21, 25, 24, 23, 22, 22, 200, 24, 28, 29, 20, 29, 28 ] );
+    assert.equal( values.q1, 22 );
+    assert.equal( values.q3, 28 );
+    assert.equal( limits.lowerLimit, 13 );
+    assert.equal( limits.upperLimit, 37 );
+  } );
+
 } );
