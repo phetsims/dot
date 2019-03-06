66635f6d22b76f5ef9e0a1c1429ecdb7fa1bf0a0
samreid <reids@colorado.edu>
2019-03-06 09:31:00 -0700

    Add Vector2Property, see https://github.com/phetsims/dot/issues/88

---
+++ js/Vector2Property.js
@@ -0,0 +1,34 @@
+// Copyright 2019, University of Colorado Boulder
+
+/**
+ * Convenience subtype of Property that takes only Vector2 values (no null values)
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( require => {
+  'use strict';
+
+  // modules
+  const dot = require( 'DOT/dot' );
+  const Property = require( 'AXON/Property' );
+  const PropertyIO = require( 'AXON/PropertyIO' );
+  const Vector2 = require( 'DOT/Vector2' );
+  const Vector2IO = require( 'DOT/Vector2IO' );
+
+  class Vector2Property extends Property {
+
+    /**
+     * @param {Vector2} initialValue
+     * @param {Object} [options]
+     */
+    constructor( initialValue, options ) {
+      options = _.extend( {
+        valueType: Vector2,
+        phetioType: PropertyIO( Vector2IO )
+      } );
+      super( initialValue, options );
+    }
+  }
+
+  return dot.register( 'Vector2Property', Vector2Property );
+} );
