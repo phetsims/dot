144ff67082a6a4e98326b755fcbe04c6b84e93ab
samreid <reids@colorado.edu>
2020-01-21 12:32:22 -0700

    Use standard form for require statements, see https://github.com/phetsims/chipper/issues/820

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
 // Copyright 2013-2019, University of Colorado Boulder
 
-define( [
-  'DOT/dot',
-  'DOT/BinPacker',
-  'DOT/Bounds2',
-  'DOT/Bounds3',
-  'DOT/Complex',
-  'DOT/ConvexHull2',
-  'DOT/DampedHarmonic',
-  'DOT/DelaunayTriangulation',
-  'DOT/Dimension2',
-  'DOT/EigenvalueDecomposition',
-  'DOT/LinearFunction',
-  'DOT/LUDecomposition',
-  'DOT/Matrix',
-  'DOT/Matrix3',
-  'DOT/Matrix4',
-  'DOT/MatrixOps3',
-  'DOT/Permutation',
-  'DOT/Plane3',
-  'DOT/QRDecomposition',
-  'DOT/Quaternion',
-  'DOT/Random',
-  'DOT/Range',
-  'DOT/Ray2',
-  'DOT/Ray3',
-  'DOT/Rectangle',
-  'DOT/SingularValueDecomposition',
-  'DOT/Sphere3',
-  'DOT/Transform3',
-  'DOT/Transform4',
-  'DOT/Utils',
-  'DOT/Vector2',
-  'DOT/Vector3',
-  'DOT/Vector4'
-], function( dot ) {
+define( require => {
   'use strict';
+  const dot = require( 'DOT/dot' );
+  require( 'DOT/BinPacker' );
+  require( 'DOT/Bounds2' );
+  require( 'DOT/Bounds3' );
+  require( 'DOT/Complex' );
+  require( 'DOT/ConvexHull2' );
+  require( 'DOT/DampedHarmonic' );
+  require( 'DOT/DelaunayTriangulation' );
+  require( 'DOT/Dimension2' );
+  require( 'DOT/EigenvalueDecomposition' );
+  require( 'DOT/LinearFunction' );
+  require( 'DOT/LUDecomposition' );
+  require( 'DOT/Matrix' );
+  require( 'DOT/Matrix3' );
+  require( 'DOT/Matrix4' );
+  require( 'DOT/MatrixOps3' );
+  require( 'DOT/Permutation' );
+  require( 'DOT/Plane3' );
+  require( 'DOT/QRDecomposition' );
+  require( 'DOT/Quaternion' );
+  require( 'DOT/Random' );
+  require( 'DOT/Range' );
+  require( 'DOT/Ray2' );
+  require( 'DOT/Ray3' );
+  require( 'DOT/Rectangle' );
+  require( 'DOT/SingularValueDecomposition' );
+  require( 'DOT/Sphere3' );
+  require( 'DOT/Transform3' );
+  require( 'DOT/Transform4' );
+  require( 'DOT/Utils' );
+  require( 'DOT/Vector2' );
+  require( 'DOT/Vector3' );
+  require( 'DOT/Vector4' );
+
   return dot;
 } );
