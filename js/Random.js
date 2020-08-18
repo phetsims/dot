678aa49700810d3b8806d59d9afd6277f82f54e6
samreid <reids@colorado.edu>
2020-08-18 14:54:39 -0600

    Convert to ES6 class, see https://github.com/phetsims/dot/issues/103

--- js/Random.js
+++ js/Random.js
@@ -10,62 +10,61 @@
  * @author Mohamed Safi
  */
 
-import inherit from '../../phet-core/js/inherit.js';
 import merge from '../../phet-core/js/merge.js';
 import dot from './dot.js';
 import Range from './Range.js';
 import Utils from './Utils.js';
 
-/**
- * Construct a Random instance.
- *
- * If you are developing a PhET Simulation, you should probably use the global `phet.joist.random` because it
- * provides built-in support for phet-io seeding and a check that it isn't used before the seed has been set.
- *
- * @param {Object} [options]
- * @constructor
- */
-function Random( options ) {
-  options = merge( {
+class Random {
 
-    // {number|null} seed for the random number generator.  When seed is null, Math.random() is used.
-    seed: null,
+  /**
+   * Construct a Random instance.
+   *
+   * If you are developing a PhET Simulation, you should probably use the global `phet.joist.random` because it
+   * provides built-in support for phet-io seeding and a check that it isn't used before the seed has been set.
+   *
+   * @param {Object} [options]
+   * @constructor
+   */
+  constructor( options ) {
+    options = merge( {
 
-    // {boolean} if true, use the seed specified statically in `phet.chipper.randomSeed`.  This value is declared
-    // in initialize-globals.js and can be overriden by PhET-iO for reproducible playback (see PhetioEngineIO.setRandomSeed).
-    staticSeed: false
+      // {number|null} seed for the random number generator.  When seed is null, Math.random() is used.
+      seed: null,
 
-  }, options );
+      // {boolean} if true, use the seed specified statically in `phet.chipper.randomSeed`.  This value is declared
+      // in initialize-globals.js and can be overriden by PhET-iO for reproducible playback (see PhetioEngineIO.setRandomSeed).
+      staticSeed: false
 
-  // If staticSeed and seed are both specified, there will be an assertion error.
-  if ( options.seed !== null && options.staticSeed ) {
-    assert && assert( false, 'cannot specify seed and staticSeed, use one or the other' );
+    }, options );
+
+    // If staticSeed and seed are both specified, there will be an assertion error.
+    if ( options.seed !== null && options.staticSeed ) {
+      assert && assert( false, 'cannot specify seed and staticSeed, use one or the other' );
+    }
+
+    // @private {number|null} initialized via setSeed below
+    this.seed = null;
+
+    // @private {function:number|null} initialized via setSeed below
+    this.seedrandom = null;
+
+    const seed = options.staticSeed ? window.phet.chipper.randomSeed : options.seed;
+    this.setSeed( seed );
   }
-
-  // @private {number|null} initialized via setSeed below
-  this.seed = null;
-
-  // @private {function:number|null} initialized via setSeed below
-  this.seedrandom = null;
-
-  const seed = options.staticSeed ? window.phet.chipper.randomSeed : options.seed;
-  this.setSeed( seed );
-}
-
-inherit( Object, Random, {
 
   /**
    * Sets the seed of the random number generator. Setting it to null reverts the random generator to Math.random()
    * @public
    * @param {number|null} seed
    */
-  setSeed: function( seed ) {
+  setSeed( seed ) {
     this.seed = seed;
 
     // If seed is provided, create a local random number generator without altering Math.random.
     // Math.seedrandom is provided by seedrandom.js, see https://github.com/davidbau/seedrandom.
     this.seedrandom = ( this.seed !== null ) ? new Math.seedrandom( this.seed + '' ) : null;
-  },
+  }
 
   /**
    * Gets the seed.
@@ -74,16 +73,16 @@ inherit( Object, Random, {
    */
   getSeed() {
     return this.seed;
-  },
+  }
 
   /**
    * Returns the next pseudo-random boolean
    * @public
    * @returns {boolean}
    */
-  nextBoolean: function() {
+  nextBoolean() {
     return this.nextDouble() >= 0.5;
-  },
+  }
 
   /**
    * Returns the next pseudo random number from this random number generator sequence.
@@ -92,19 +91,19 @@ inherit( Object, Random, {
    * @param {number} n
    * @returns {number} - an integer
    */
-  nextInt: function( n ) {
+  nextInt( n ) {
     const value = this.nextDouble() * n;
     return value | 0; // convert to int by removing the decimal places
-  },
+  }
 
   /**
    * Randomly select a random integer between min and max (inclusive).
-    *@public
+   *@public
    * @param {number} min - must be an integer
    * @param {number} max - must be an integer
    * @returns {number} an integer between min and max, inclusive
    */
-  nextIntBetween: function( min, max ) {
+  nextIntBetween( min, max ) {
 
     assert && assert( arguments.length === 2, 'nextIntBetween must have exactly 2 arguments' );
     assert && assert( Utils.isInteger( min ), 'min must be an integer: ' + min );
@@ -112,7 +111,7 @@ inherit( Object, Random, {
 
     const range = max - min;
     return this.nextInt( range + 1 ) + min;
-  },
+  }
 
   /**
    * Randomly select one element from the given array.
@@ -120,11 +119,11 @@ inherit( Object, Random, {
    * @param {Object[]} array - the array from which one element will be selected, must have at least one element
    * @returns {Object} - the selected element from the array
    */
-  sample: function( array ) {
+  sample( array ) {
     assert && assert( array.length > 0, 'Array should have at least 1 item.' );
     const index = this.nextIntBetween( 0, array.length - 1 );
     return array[ index ];
-  },
+  }
 
   /**
    * Creates an array of shuffled values, using a version of the Fisher-Yates shuffle.  Adapted from lodash-2.4.1 by
@@ -133,7 +132,7 @@ inherit( Object, Random, {
    * @param {Array} array - the array which will be shuffled
    * @returns {Array} a new array with all the same elements in the passed-in array, in randomized order.
    */
-  shuffle: function( array ) {
+  shuffle( array ) {
     assert && assert( array, 'Array should exist' );
     const self = this;
     let index = -1;
@@ -145,7 +144,7 @@ inherit( Object, Random, {
       result[ rand ] = value;
     } );
     return result;
-  },
+  }
 
   /**
    * Returns the next pseudo random number from this random number generator sequence in the range [0, 1)
@@ -153,9 +152,9 @@ inherit( Object, Random, {
    * @public
    * @returns {number} - the random number
    */
-  nextDouble: function() {
+  nextDouble() {
     return this.seed === null ? Math.random() : this.seedrandom(); // eslint-disable-line bad-sim-text
-  },
+  }
 
   /**
    * Randomly selects a double in the range [min,max).
@@ -164,12 +163,12 @@ inherit( Object, Random, {
    * @param {number} max
    * @returns {number}
    */
-  nextDoubleBetween: function( min, max ) {
+  nextDoubleBetween( min, max ) {
     assert && assert( min < max, 'min must be < max' );
     const value = min + this.nextDouble() * ( max - min );
     assert && assert( value >= min && value < max, `value out of range: ${value}` );
     return value;
-  },
+  }
 
   /**
    * Returns the next gaussian-distributed random number from this random number generator sequence.
@@ -177,9 +176,9 @@ inherit( Object, Random, {
    * @public
    * @returns {number}
    */
-  nextGaussian: function() {
+  nextGaussian() {
     return Utils.boxMullerTransform( 0, 1, this );
-  },
+  }
 
   /**
    * Gets the next random double in a Range.
@@ -199,7 +198,7 @@ inherit( Object, Random, {
       return range.min;
     }
   }
-} );
+}
 
 dot.register( 'Random', Random );
 
