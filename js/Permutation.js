dc150bc04b8e5a60af6d984c47e94ff26ea35196
Jonathan Olson <jonathan.olson@colorado.edu>
2020-10-09 12:00:54 -0600

    Upgrading Permutation for https://github.com/phetsims/tasks/issues/1044

--- js/Permutation.js
+++ js/Permutation.js
@@ -7,34 +7,143 @@
  */
 
 import isArray from '../../phet-core/js/isArray.js';
-import dot from './dot.js';
 import './Utils.js';
+import dot from './dot.js';
 
-// Creates a permutation that will rearrange a list so that newList[i] = oldList[permutation[i]]
-function Permutation( indices ) {
-  this.indices = indices;
+class Permutation {
+  /**
+   * Creates a permutation that will rearrange a list so that newList[i] = oldList[permutation[i]]
+   *
+   * @param {Array.<number>} indices
+   */
+  constructor( indices ) {
+    // @public {Array.<number>}
+    this.indices = indices;
+  }
+
+  /**
+   * @public
+   *
+   * @returns {number}
+   */
+  size() {
+    return this.indices.length;
+  }
+
+  /**
+   * Applies the permutation, returning either a new array or number (whatever was provided).
+   * @public
+   *
+   * @param {Array.<number>|number} arrayOrInt
+   * @returns {Array.<number>|number}
+   */
+  apply( arrayOrInt ) {
+    if ( isArray( arrayOrInt ) ) {
+      if ( arrayOrInt.length !== this.size() ) {
+        throw new Error( 'Permutation length ' + this.size() + ' not equal to list length ' + arrayOrInt.length );
+      }
+
+      // permute it as an array
+      const result = new Array( arrayOrInt.length );
+      for ( let i = 0; i < arrayOrInt.length; i++ ) {
+        result[ i ] = arrayOrInt[ this.indices[ i ] ];
+      }
+      return result;
+    }
+    else {
+      // permute a single index
+      return this.indices[ arrayOrInt ];
+    }
+  }
+
+  /**
+   * Creates a new permutation that is the inverse of this.
+   * @public
+   *
+   * @returns {Permutation}
+   */
+  inverted() {
+    const newPermutation = new Array( this.size() );
+    for ( let i = 0; i < this.size(); i++ ) {
+      newPermutation[ this.indices[ i ] ] = i;
+    }
+    return new Permutation( newPermutation );
+  }
+
+  /**
+   * @public
+   *
+   * @param {Array.<number>} indices
+   * @returns {Array.<Permutation>}
+   */
+  withIndicesPermuted( indices ) {
+    const result = [];
+    const self = this;
+    Permutation.forEachPermutation( indices, function( integers ) {
+      const oldIndices = self.indices;
+      const newPermutation = oldIndices.slice( 0 );
+
+      for ( let i = 0; i < indices.length; i++ ) {
+        newPermutation[ indices[ i ] ] = oldIndices[ integers[ i ] ];
+      }
+      result.push( new Permutation( newPermutation ) );
+    } );
+    return result;
+  }
+
+  /**
+   * @public
+   *
+   * @returns {string}
+   */
+  toString() {
+    return 'P[' + this.indices.join( ', ' ) + ']';
+  }
+
+  /**
+   * Creates an identity permutation of a given size.
+   * @public
+   *
+   * @param {number} size
+   * @returns {Permutation}
+   */
+  static identity( size ) {
+    assert && assert( size >= 0 );
+    const indices = new Array( size );
+    for ( let i = 0; i < size; i++ ) {
+      indices[ i ] = i;
+    }
+    return new Permutation( indices );
+  }
+
+  /**
+   * Lists all permutations that have a given size
+   * @public
+   *
+   * @param {number} size
+   * @returns {Array.<Permutation>}
+   */
+  static permutations( size ) {
+    const result = [];
+    Permutation.forEachPermutation( dot.rangeInclusive( 0, size - 1 ), function( integers ) {
+      result.push( new Permutation( integers ) );
+    } );
+    return result;
+  }
+
+  /**
+   * Calls a callback on every single possible permutation of the given Array
+   * @public
+   *
+   * @param {Array.<*>} array
+   * @param {function(Array.<*>)} callback - Called on each permuted version of the array possible
+   */
+  static forEachPermutation( array, callback ) {
+    recursiveForEachPermutation( array, [], callback );
+  }
 }
 
 dot.register( 'Permutation', Permutation );
-
-// An identity permutation with a specific number of elements
-Permutation.identity = function( size ) {
-  assert && assert( size >= 0 );
-  const indices = new Array( size );
-  for ( let i = 0; i < size; i++ ) {
-    indices[ i ] = i;
-  }
-  return new Permutation( indices );
-};
-
-// lists all permutations that have a given size
-Permutation.permutations = function( size ) {
-  const result = [];
-  Permutation.forEachPermutation( dot.rangeInclusive( 0, size - 1 ), function( integers ) {
-    result.push( new Permutation( integers ) );
-  } );
-  return result;
-};
 
 /**
  * Call our function with each permutation of the provided list PREFIXED by prefix, in lexicographic order
@@ -63,76 +172,5 @@ function recursiveForEachPermutation( array, prefix, callback ) {
     }
   }
 }
-
-Permutation.forEachPermutation = function( array, callback ) {
-  recursiveForEachPermutation( array, [], callback );
-};
-
-Permutation.prototype = {
-  constructor: Permutation,
-
-  size: function() {
-    return this.indices.length;
-  },
-
-  apply: function( arrayOrInt ) {
-    if ( isArray( arrayOrInt ) ) {
-      if ( arrayOrInt.length !== this.size() ) {
-        throw new Error( 'Permutation length ' + this.size() + ' not equal to list length ' + arrayOrInt.length );
-      }
-
-      // permute it as an array
-      const result = new Array( arrayOrInt.length );
-      for ( let i = 0; i < arrayOrInt.length; i++ ) {
-        result[ i ] = arrayOrInt[ this.indices[ i ] ];
-      }
-      return result;
-    }
-    else {
-      // permute a single index
-      return this.indices[ arrayOrInt ];
-    }
-  },
-
-  // The inverse of this permutation
-  inverted: function() {
-    const newPermutation = new Array( this.size() );
-    for ( let i = 0; i < this.size(); i++ ) {
-      newPermutation[ this.indices[ i ] ] = i;
-    }
-    return new Permutation( newPermutation );
-  },
-
-  withIndicesPermuted: function( indices ) {
-    const result = [];
-    const self = this;
-    Permutation.forEachPermutation( indices, function( integers ) {
-      const oldIndices = self.indices;
-      const newPermutation = oldIndices.slice( 0 );
-
-      for ( let i = 0; i < indices.length; i++ ) {
-        newPermutation[ indices[ i ] ] = oldIndices[ integers[ i ] ];
-      }
-      result.push( new Permutation( newPermutation ) );
-    } );
-    return result;
-  },
-
-  toString: function() {
-    return 'P[' + this.indices.join( ', ' ) + ']';
-  }
-};
-
-Permutation.testMe = function( console ) {
-  const a = new Permutation( [ 1, 4, 3, 2, 0 ] );
-  console.log( a.toString() );
-
-  const b = a.inverted();
-  console.log( b.toString() );
-
-  console.log( b.withIndicesPermuted( [ 0, 3, 4 ] ).toString() );
-
-  console.log( Permutation.permutations( 4 ).toString() );
-};
 
 export default Permutation;
