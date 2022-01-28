bce301e124d075942924bda2c2d98481223a5d64
Jonathan Olson <jonathan.olson@colorado.edu>
2022-01-28 08:58:28 -0700

    Import renaming, see phetsims/chipper#1177

--- js/dot-main.js
+++ js/dot-main.js
@@ -1,13 +1,12 @@
-import axon from '../../axon/js/main.js';
-import phetCore from '../../phet-core/js/main.js';
-import dot from './main.js';
+// Copyright 2016-2022, University of Colorado Boulder
 
-// Copyright 2016-2019, University of Colorado Boulder
+import axon from '../../axon/js/main.js'; // eslint-disable-line default-import-match-filename
+import phetCore from '../../phet-core/js/main.js'; // eslint-disable-line default-import-match-filename
+import dot from './main.js'; // eslint-disable-line default-import-match-filename
 
 if ( !window.hasOwnProperty( '_' ) ) {
   throw new Error( 'Underscore/Lodash not found: _' );
 }
-
 
 window.axon = axon;
 window.dot = dot;
