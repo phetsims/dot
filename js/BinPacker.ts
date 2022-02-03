85b586c61fcb0740ca05fef267b9ad9e749f9fcc
Jonathan Olson <jonathan.olson@colorado.edu>
2022-02-03 12:32:14 -0700

    Sprites => TypeScript (and associated files)

--- js/BinPacker.ts
+++ js/BinPacker.ts
@@ -32,25 +32,22 @@ import Bounds2 from './Bounds2.js';
 import dot from './dot.js';
 
 class BinPacker {
+
+  rootBin: Bin;
+
   /**
    * Creates a BinPacker with the specified containing bounds.
-   * @public
    *
-   * @param {Bounds2} bounds - The available bounds to pack bins inside.
+   * @param bounds - The available bounds to pack bins inside.
    */
-  constructor( bounds ) {
-    this.rootBin = new dot.BinPacker.Bin( bounds, null );
+  constructor( bounds: Bounds2 ) {
+    this.rootBin = new Bin( bounds, null );
   }
 
   /**
    * Allocates a bin with the specified width and height if possible (returning a {Bin}), otherwise returns null.
-   * @public
-   *
-   * @param {number} width
-   * @param {number} height
-   * @returns {Bin|null}
    */
-  allocate( width, height ) {
+  allocate( width: number, height: number ): Bin | null {
     // find a leaf bin that has available room (or null)
     const bin = this.rootBin.findAvailableBin( width, height );
 
@@ -70,24 +67,19 @@ class BinPacker {
 
   /**
    * Deallocates a bin, so that its area can be reused by future allocations.
-   * @public
    *
-   * @param {Bin} bin - The bin that was returned from allocate().
+   * @param bin - The bin that was returned from allocate().
    */
-  deallocate( bin ) {
+  deallocate( bin: Bin ) {
     bin.unuse();
   }
 
-  /**
-   * @private, for debugging purposes
-   * @returns {string}
-   */
-  toString() {
+  toString(): string {
     let result = '';
 
     let padding = '';
 
-    function binTree( bin ) {
+    function binTree( bin: Bin ) {
       result += `${padding + bin.toString()}\n`;
       padding = `${padding}  `;
       _.each( bin.children, binTree );
@@ -98,44 +90,43 @@ class BinPacker {
 
     return result;
   }
+
+  static Bin: typeof Bin;
 }
 
 dot.register( 'BinPacker', BinPacker );
 
 class Bin {
+
+  // Our containing bounds
+  bounds: Bounds2;
+
+  // Parent bin, if applicable
+  private parent: Bin | null;
+
+  // Whether our children are responsible for our area
+  private isSplit: boolean
+
+  // Whether we are marked as a bin that is used
+  private isUsed: boolean;
+
+  children: Bin[]; // (dot-internal)
+
   /**
    * A rectangular bin that can be used itself or split into sub-bins.
-   * @public
-   *
-   * @param {Bounds2} bounds
-   * @param {Bin|null} parent
    */
-  constructor( bounds, parent ) {
-    // @public {Bounds2} - Our containing bounds
+  constructor( bounds: Bounds2, parent: Bin | null ) {
     this.bounds = bounds;
-
-    // @private {Bin|null} - Parent bin, if applicable
     this.parent = parent;
-
-    // @private {boolean} - Whether our children are responsible for our area
     this.isSplit = false;
-
-    // @private {boolean} - Whether we are marked as a bin that is used
     this.isUsed = false;
-
-    // @private {Array.<Bin>}
     this.children = [];
   }
 
   /**
-   * Finds an unused bin with open area that is at least width-x-height in size.
-   * @private
-   *
-   * @param {number} width
-   * @param {number} height
-   * @returns {Bin|null}
+   * Finds an unused bin with open area that is at least width-x-height in size. (dot-internal)
    */
-  findAvailableBin( width, height ) {
+  findAvailableBin( width: number, height: number ): Bin | null {
     assert && assert( width > 0 && height > 0, 'Empty bin requested?' );
 
     // If we are marked as used ourself, we can't be used
@@ -164,13 +155,9 @@ class Bin {
   }
 
   /**
-   * Splits this bin into multiple child bins, and returns the child with the dimensions (width,height).
-   * @private
-   *
-   * @param {number} width
-   * @param {number} height
+   * Splits this bin into multiple child bins, and returns the child with the dimensions (width,height). (dot-internal)
    */
-  split( width, height ) {
+  split( width: number, height: number ) {
     assert && assert( this.bounds.width >= width && this.bounds.height >= height,
       'Bin does not have space' );
     assert && assert( !this.isSplit, 'Bin should not be re-split' );
@@ -209,23 +196,22 @@ class Bin {
     const rightBounds = new Bounds2( splitX, this.bounds.minY, this.bounds.maxX, splitY );
     const bottomBounds = new Bounds2( this.bounds.minX, splitY, this.bounds.maxX, this.bounds.maxY );
 
-    const mainBin = new dot.BinPacker.Bin( mainBounds, this );
+    const mainBin = new Bin( mainBounds, this );
     this.children.push( mainBin );
 
     // only add right/bottom if they take up area
     if ( rightBounds.hasNonzeroArea() ) {
-      this.children.push( new dot.BinPacker.Bin( rightBounds, this ) );
+      this.children.push( new Bin( rightBounds, this ) );
     }
     if ( bottomBounds.hasNonzeroArea() ) {
-      this.children.push( new dot.BinPacker.Bin( bottomBounds, this ) );
+      this.children.push( new Bin( bottomBounds, this ) );
     }
 
     return mainBin;
   }
 
   /**
-   * Mark this bin as used.
-   * @private
+   * Mark this bin as used. (dot-internal)
    */
   use() {
     assert && assert( !this.isSplit, 'Should not mark a split bin as used' );
@@ -235,8 +221,7 @@ class Bin {
   }
 
   /**
-   * Mark this bin as not used, and attempt to collapse split parents if all children are unused.
-   * @private
+   * Mark this bin as not used, and attempt to collapse split parents if all children are unused. (dot-internal)
    */
   unuse() {
     assert && assert( this.isUsed, 'Can only unuse a used instance' );
@@ -250,9 +235,8 @@ class Bin {
    * If our bin can be collapsed (it is split and has children that are not used AND not split), then we will become
    * not split, and will remove our children. If successful, it will also call this on our parent, fully attempting
    * to clean up unused data structures.
-   * @private
    */
-  attemptToCollapse() {
+  private attemptToCollapse() {
     assert && assert( this.isSplit, 'Should only attempt to collapse split bins' );
 
     // Bail out if a single child isn't able to be collapsed. If it is not split or used, it won't have any children
@@ -273,11 +257,7 @@ class Bin {
     this.parent && this.parent.attemptToCollapse();
   }
 
-  /**
-   * @private, for debugging purposes
-   * @returns {string}
-   */
-  toString() {
+  toString(): string {
     return this.bounds.toString() + ( this.isUsed ? ' used' : '' );
   }
 }
@@ -285,3 +265,4 @@ class Bin {
 BinPacker.Bin = Bin;
 
 export default BinPacker;
+export { Bin };
