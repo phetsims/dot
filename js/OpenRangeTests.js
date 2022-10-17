cd7f877af059fc1a3e411bd5b72a7523955ab21e
Martin Veillette <martin.veillette@gmail.com>
2022-10-17 12:38:13 -0400

    fix string: contians -> contains
    Signed-off-by: Martin Veillette <martin.veillette@gmail.com>

--- js/OpenRangeTests.js
+++ js/OpenRangeTests.js
@@ -19,7 +19,7 @@ QUnit.test( 'half open min', assert => {
   assert.notOk( testMinOpenRange.contains( 1 ), '(1, 10] does not contain 1' );
   assert.notOk( testMinOpenRange.intersects( new Range( 0, 1 ) ), '(1, 10] does not intersect [0, 1]' );
   assert.notOk( testMinOpenRange.containsRange( new Range( 1, 2 ), '(1, 10] does not contain [1, 2]' ) );
-  assert.ok( testMinOpenRange.contains( 1.000000001 ), '(1, 10] contians 1.000000001' );
+  assert.ok( testMinOpenRange.contains( 1.000000001 ), '(1, 10] contains 1.000000001' );
   assert.ok( testMinOpenRange.intersects( new Range( 0, 1.000000001 ) ), '(1, 10] intersects [0, 1.000000001]' );
   assert.ok( testMinOpenRange.containsRange( new Range( 1.000000001, 2 ) ), '(1, 10] contains [1.000000001, 2]' );
 } );
@@ -29,9 +29,9 @@ QUnit.test( 'half open max', assert => {
   assert.notOk( maxOpenRange.contains( 10 ), '[1, 10) does not contain 10' );
   assert.notOk( maxOpenRange.intersects( new Range( 10, 11 ) ), '[1, 10) does not intersect [10,11]' );
   assert.notOk( maxOpenRange.containsRange( new Range( 9, 10 ), '[1, 10) does not contiain [9, 10]' ) );
-  assert.ok( maxOpenRange.contains( 9.999999999 ), '[1, 10) contians 9.999999999' );
+  assert.ok( maxOpenRange.contains( 9.999999999 ), '[1, 10) contains 9.999999999' );
   assert.ok( maxOpenRange.intersects( new Range( 9.999999999, 11 ) ), '[1, 10) intersects [9.999999999, 11]' );
-  assert.ok( maxOpenRange.containsRange( new Range( 9, 9.999999999 ) ), '[1, 10) contians [9, 9.999999999]' );
+  assert.ok( maxOpenRange.containsRange( new Range( 9, 9.999999999 ) ), '[1, 10) contains [9, 9.999999999]' );
 } );
 
 QUnit.test( 'fully open range', assert => {
@@ -42,11 +42,11 @@ QUnit.test( 'fully open range', assert => {
   assert.notOk( openRange.intersects( new Range( 10, 11 ) ), '(1, 10) does not intersect [10,11]' );
   assert.notOk( openRange.containsRange( new Range( 9, 10 ), '(1, 10) does not contiain [9, 10]' ) );
   assert.notOk( openRange.containsRange( new Range( 1, 2 ), '(1, 10) does not contain [1, 2]' ) );
-  assert.ok( openRange.contains( 1.000000001 ), '(1, 10) contians 1.000000001' );
-  assert.ok( openRange.contains( 9.999999999 ), '(1, 10) contians 9.999999999' );
+  assert.ok( openRange.contains( 1.000000001 ), '(1, 10) contains 1.000000001' );
+  assert.ok( openRange.contains( 9.999999999 ), '(1, 10) contains 9.999999999' );
   assert.ok( openRange.intersects( new Range( 0, 1.000000001 ) ), '(1, 10) intersects [0, 1.000000001]' );
   assert.ok( openRange.intersects( new Range( 9.999999999, 11 ) ), '(1, 10) intersects [9.999999999, 11]' );
-  assert.ok( openRange.containsRange( new Range( 9, 9.999999999 ) ), '(1, 10) contians [9, 9.999999999]' );
+  assert.ok( openRange.containsRange( new Range( 9, 9.999999999 ) ), '(1, 10) contains [9, 9.999999999]' );
   assert.ok( openRange.containsRange( new Range( 1.000000001, 2 ) ), '(1, 10) contains [1.000000001, 2]' );
 } );
 
