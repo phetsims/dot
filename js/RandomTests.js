bf884f163ad77fe3304c5cf88e912db47ce0f1a5
Sam Reid <reids@colorado.edu>
2023-04-29 14:23:14 -0600

    Move sampling tests to dot, see https://github.com/phetsims/center-and-variability/issues/45

--- js/RandomTests.js
+++ js/RandomTests.js
@@ -7,6 +7,7 @@
  */
 
 import Bounds2 from './Bounds2.js';
+import dotRandom from './dotRandom.js';
 import Random from './Random.js';
 
 QUnit.module( 'Random' );
@@ -19,4 +20,32 @@ QUnit.test( 'Random.nextPointInBounds', assert => {
   for ( let i = 0; i < 100; i++ ) {
     assert.ok( bounds.containsPoint( random.nextPointInBounds( bounds ) ), `random point is in bounds: ${i}` );
   }
+} );
+
+// Test that the sampling engine is working properly
+QUnit.test( 'sample probabilities', assert => {
+  assert.ok( 'first test' );
+
+  const testWeights = weights => {
+
+    const array = new Array( weights.length );
+    _.fill( array, 0, 0, array.length );
+    for ( let i = 0; i < 50000; i++ ) {
+      const index = dotRandom.sampleProbabilities( weights );
+      array[ index ]++;
+    }
+
+    const inputNormalized = weights.map( element => ( element / _.sum( weights ) ) );
+    const resultNormalized = array.map( element => ( element / _.sum( array ) ) );
+
+    let ok = true;
+    for ( let i = 0; i < weights.length; i++ ) {
+      if ( Math.abs( inputNormalized[ i ] - resultNormalized[ i ] ) > 0.1 ) {
+        ok = false;
+      }
+    }
+    assert.ok( ok, `inputNormalized: ${inputNormalized.join( ',' )}, resultNormalized: ${resultNormalized.join( ',' )}` );
+  };
+  testWeights( [ 1, 2, 3, 4 ] );
+  testWeights( [ 0, 1, 0, 0 ] );
 } );
