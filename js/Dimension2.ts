9b1dc54d90b771e3fcbdd73a9ef7b3b9a2494db2
pixelzoom <cmalley@pixelzoom.com>
2023-11-02 10:24:05 -0600

    convert Dimension2 to TypeScript, https://github.com/phetsims/dot/issues/123

--- js/Dimension2.ts
+++ js/Dimension2.ts
@@ -1,7 +1,7 @@
 // Copyright 2013-2021, University of Colorado Boulder
 
 /**
- * Basic width and height, like a Bounds2 but without the location defined.
+ * Dimension2 is a basic width and height, like a Bounds2 but without the position defined.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
@@ -9,43 +9,32 @@
 import Bounds2 from './Bounds2.js';
 import dot from './dot.js';
 
-class Dimension2 {
-  /**
-   * Creates a 2-dimensional size with a width and height
-   * @public
-   *
-   * @param {number} width
-   * @param {number} height
-   */
-  constructor( width, height ) {
-    // @public {number} - Width of the dimension
-    this.width = width;
+export default class Dimension2 {
 
-    // @public {number} - Height of the dimension
+  // Width of the dimension
+  public width: number;
+
+  // Height of the dimension
+  public height: number;
+
+  public constructor( width: number, height: number ) {
+    this.width = width;
     this.height = height;
   }
 
   /**
    * Debugging string for the dimension.
-   * @public
-   *
-   * @returns {string}
    */
-  toString() {
+  public toString(): string {
     return `[${this.width}w, ${this.height}h]`;
   }
 
   /**
    * Sets this dimension to be a copy of another dimension.
-   * @public
-   *
    * This is the mutable form of the function copy(). This will mutate (change) this dimension, in addition to returning
    * this dimension itself.
-   *
-   * @param {Dimension2} dimension
-   * @returns {Dimension2}
    */
-  set( dimension ) {
+  public set( dimension: Dimension2 ): Dimension2 {
     this.width = dimension.width;
     this.height = dimension.height;
     return this;
@@ -53,40 +42,29 @@ class Dimension2 {
 
   /**
    * Sets the width of the dimension, returning this.
-   * @public
-   *
-   * @param {number} width
-   * @returns {Dimension2}
    */
-  setWidth( width ) {
+  public setWidth( width: number ): Dimension2 {
     this.width = width;
     return this;
   }
 
   /**
    * Sets the height of the dimension, returning this.
-   * @public
-   *
-   * @param {number} height
-   * @returns {Dimension2}
    */
-  setHeight( height ) {
+  public setHeight( height: number ): Dimension2 {
     this.height = height;
     return this;
   }
 
   /**
    * Creates a copy of this dimension, or if a dimension is passed in, set that dimension's values to ours.
-   * @public
-   *
    * This is the immutable form of the function set(), if a dimension is provided. This will return a new dimension,
    * and will not modify this dimension.
    *
-   * @param {Dimension2} [dimension] - If not provided, creates a new Dimension2 with filled in values. Otherwise, fills
-   *                                   in the values of the provided dimension so that it equals this dimension.
-   * @returns {Dimension2}
+   * @param [dimension] - If not provided, creates a new Dimension2 with filled in values. Otherwise, fills
+   *                      in the values of the provided dimension so that it equals this dimension.
    */
-  copy( dimension ) {
+  public copy( dimension?: Dimension2 ): Dimension2 {
     if ( dimension ) {
       return dimension.set( this );
     }
@@ -97,22 +75,17 @@ class Dimension2 {
 
   /**
    * Swap width and height and return a new Dimension2
-   * @returns {Dimension2}
-   * @public
    */
-  swapped() {
+  public swapped(): Dimension2 {
     return new Dimension2( this.height, this.width );
   }
 
   /**
    * Creates a Bounds2 from this dimension based on passing in the minimum (top-left) corner as (x,y).
-   * @public
-   *
-   * @param {number} [x] - Minimum x coordinate of the bounds, or 0 if not provided.
-   * @param {number} [y] - Minimum y coordinate of the bounds, or 0 if not provided.
-   * @returns {Bounds2}
+   * @param [x] - Minimum x coordinate of the bounds, or 0 if not provided.
+   * @param [y] - Minimum y coordinate of the bounds, or 0 if not provided.
    */
-  toBounds( x, y ) {
+  public toBounds( x?: number, y?: number ): Bounds2 {
     x = x !== undefined ? x : 0;
     y = y !== undefined ? y : 0;
     return new Bounds2( x, y, this.width + x, this.height + y );
@@ -120,16 +93,10 @@ class Dimension2 {
 
   /**
    * Exact equality comparison between this dimension and another dimension.
-   * @public
-   *
-   * @param {Dimension2} other
-   * @returns {boolean} - Whether the two dimensions have equal width and height
    */
-  equals( other ) {
-    return this.width === other.width && this.height === other.height;
+  public equals( that: Dimension2 ): boolean {
+    return this.width === that.width && this.height === that.height;
   }
 }
 
 dot.register( 'Dimension2', Dimension2 );
-
-export default Dimension2;
