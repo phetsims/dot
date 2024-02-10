52c57f59729b480115b9f2e0b579c5a281eba29e
Jonathan Olson <jonathan.olson@colorado.edu>
2024-02-09 18:34:46 -0700

    phet-lib and associated imports changes prepping for npm package

---
+++ js/imports.ts
@@ -0,0 +1,60 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * "Barrel" file for dot, so that we can export all of the API of the repo.
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
+export { default as BinPacker, Bin } from './BinPacker.js';
+export { default as Bounds2 } from './Bounds2.js';
+export type { Bounds2StateObject } from './Bounds2.js';
+export { default as Bounds3 } from './Bounds3.js';
+export { default as Combination } from './Combination.js';
+export { default as CompletePiecewiseLinearFunction } from './CompletePiecewiseLinearFunction.js';
+export { default as Complex } from './Complex.js';
+export { default as ConvexHull2 } from './ConvexHull2.js';
+export { default as DampedHarmonic } from './DampedHarmonic.js';
+export { default as DelaunayTriangulation } from './DelaunayTriangulation.js';
+export { default as Dimension2 } from './Dimension2.js';
+export type { Dimension2StateObject } from './Dimension2.js';
+export { default as dot } from './dot.js';
+export { default as dotRandom } from './dotRandom.js';
+export { default as EigenvalueDecomposition } from './EigenvalueDecomposition.js';
+export { default as LinearFunction } from './LinearFunction.js';
+export { default as LUDecomposition } from './LUDecomposition.js';
+export { default as LUDecompositionDecimal } from './LUDecompositionDecimal.js';
+export { default as Matrix } from './Matrix.js';
+export { default as Matrix3, Matrix3Type, m3 } from './Matrix3.js';
+export type { Matrix3StateObject } from './Matrix3.js';
+export { default as Matrix4 } from './Matrix4.js';
+export { default as OpenRange } from './OpenRange.js';
+export { default as Permutation } from './Permutation.js';
+export { default as PiecewiseLinearFunction } from './PiecewiseLinearFunction.js';
+export { default as Plane3 } from './Plane3.js';
+export { default as QRDecomposition } from './QRDecomposition.js';
+export { default as Quaternion } from './Quaternion.js';
+export { default as Random } from './Random.js';
+export { default as Range } from './Range.js';
+export type { TRange, RangeStateObject } from './Range.js';
+export { default as RangeWithValue } from './RangeWithValue.js';
+export { default as Ray2 } from './Ray2.js';
+export { default as Ray3 } from './Ray3.js';
+export { default as Rectangle } from './Rectangle.js';
+export { default as RunningAverage } from './RunningAverage.js';
+export { default as SingularValueDecomposition } from './SingularValueDecomposition.js';
+export { default as Sphere3 } from './Sphere3.js';
+export { default as Stats } from './Stats.js';
+export { default as toSVGNumber } from './toSVGNumber.js';
+export { default as Transform1 } from './Transform1.js';
+export { default as Transform3 } from './Transform3.js';
+export { default as Transform4 } from './Transform4.js';
+export { default as UnivariatePolynomial } from './UnivariatePolynomial.js';
+export { default as DotUtils } from './Utils.js';
+export { default as Vector2, v2 } from './Vector2.js';
+export type { Vector2StateObject } from './Vector2.js';
+export { default as Vector2Property } from './Vector2Property.js';
+export type { Vector2PropertyOptions } from './Vector2Property.js';
+export { default as Vector3, v3 } from './Vector3.js';
+export type { Vector3StateObject } from './Vector3.js';
+export { default as Vector4, v4 } from './Vector4.js';
