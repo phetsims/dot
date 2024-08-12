ed6f8a9f3b669eb4c7db4d2b55e8ebb7a6f55d8b
AgustinVallejo <agustin.vallejo@udea.edu.co>
2024-08-12 17:02:44 -0500

    Factoring out find root function into dot, see https://github.com/phetsims/density-buoyancy-common/issues/317

---
+++ js/findRoot.ts
@@ -0,0 +1,38 @@
+// Copyright 2020-2024, University of Colorado Boulder
+/**
+ * Hybrid root-finding given our constraints (guaranteed interval, value/derivative). Combines Newton's and bisection.
+ *
+ * @author Jonathan Olson (PhET Interactive Simulations)
+ */
+export function findRoot( minX: number, maxX: number, tolerance: number, valueFunction: ( n: number ) => number, derivativeFunction: ( n: number ) => number ): number {
+  let x = ( minX + maxX ) / 2;
+
+  let y;
+  let dy;
+
+  while ( Math.abs( y = valueFunction( x ) ) > tolerance ) {
+    dy = derivativeFunction( x );
+
+    if ( y < 0 ) {
+      minX = x;
+    }
+    else {
+      maxX = x;
+    }
+
+    // Newton's method first
+    x -= y / dy;
+
+    // Bounded to be bisection at the very least
+    if ( x <= minX || x >= maxX ) {
+      x = ( minX + maxX ) / 2;
+
+      // Check to see if it's impossible to pass our tolerance
+      if ( x === minX || x === maxX ) {
+        break;
+      }
+    }
+  }
+
+  return x;
+}
