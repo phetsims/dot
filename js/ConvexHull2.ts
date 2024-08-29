aadc7eba29de476471aa9dec19660183c62771e3
Sam Reid <reids@colorado.edu>
2024-08-29 15:40:11 -0600

    Convert to *.ts, see https://github.com/phetsims/density-buoyancy-common/issues/377

--- js/ConvexHull2.ts
+++ js/ConvexHull2.ts
@@ -34,15 +34,12 @@
  */
 
 import dot from './dot.js';
+import Vector2 from './Vector2.js';
 
 /**
  * counter-clockwise turn if > 0, clockwise turn if < 0, collinear if === 0.
- * @param {Vector2} p1
- * @param {Vector2} p2
- * @param {Vector2} p3
- * @returns {number}
  */
-function ccw( p1, p2, p3 ) {
+function ccw( p1: Vector2, p2: Vector2, p3: Vector2 ): number {
   return p2.minus( p1 ).crossScalar( p3.minus( p1 ) );
 }
 
@@ -52,21 +49,19 @@ const ConvexHull2 = {
   /**
    * Given multiple points, this performs a Graham Scan (http://en.wikipedia.org/wiki/Graham_scan) to identify an
    * ordered list of points which define the minimal polygon that contains all of the points.
-   * @public
    *
-   * @param {Array.<Vector2>} points
-   * @param {boolean} includeCollinear - If a point is along an edge of the convex hull (not at one of its vertices),
+   * @param points
+   * @param includeCollinear - If a point is along an edge of the convex hull (not at one of its vertices),
    *                                     should it be included?
-   * @returns {Array.<Vector2>}
    */
-  grahamScan: ( points, includeCollinear ) => {
+  grahamScan: ( points: Vector2[], includeCollinear: boolean ): Vector2[] => {
     if ( points.length <= 2 ) {
       return points;
     }
 
     // find the point 'p' with the lowest y value
     let minY = Number.POSITIVE_INFINITY;
-    let p = null;
+    let p: Vector2 | null = null;
     _.each( points, point => {
       if ( point.y <= minY ) {
         // if two points have the same y value, take the one with the lowest x
@@ -84,20 +79,20 @@ const ConvexHull2 = {
 
     // sorts the points by their angle. Between 0 and PI
     points = _.sortBy( points, point => {
-      return point.minus( p ).angle;
+      return point.minus( p! ).angle;
     } );
 
     // remove p from points (relies on the above statement making a defensive copy)
     points.splice( _.indexOf( points, p ), 1 );
 
     // our result array
-    const result = [ p ];
+    const result: Vector2[] = [ p! ];
 
     _.each( points, point => {
       // ignore points equal to our starting point
-      if ( p.x === point.x && p.y === point.y ) { return; }
+      if ( p!.x === point.x && p!.y === point.y ) { return; }
 
-      function isRightTurn() {
+      function isRightTurn(): boolean {
         if ( result.length < 2 ) {
           return false;
         }
