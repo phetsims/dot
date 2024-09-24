b3fab63d81c0740751f68a549365077f8fa43098
phet-dev <phetadmin@gmail.com>
2024-09-24 03:29:43 -0600

    update copyright dates from daily grunt work

--- js/UnivariatePolynomial.ts
+++ js/UnivariatePolynomial.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Handles a univariate polynomial (a polynomial with one variable), like 2x^2 + 6x + 4.
