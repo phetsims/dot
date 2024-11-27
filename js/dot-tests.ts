ab745ea167652a4e33765910fc1fb16bba627b11
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-11-27 15:23:51 -0700

    move browser files in chipper into a sub directory, https://github.com/phetsims/chipper/issues/1534

--- js/dot-tests.ts
+++ js/dot-tests.ts
@@ -6,7 +6,7 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
-import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+import qunitStart from '../../chipper/js/browser/sim-tests/qunitStart.js';
 import './BinPackerTests.js';
 import './Bounds2Tests.js';
 import './ComplexTests.js';
