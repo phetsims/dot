d1f7ba7d17f41d341f176dbcbba25332d9076b6d
pixelzoom <cmalley@pixelzoom.com>
2025-02-13 10:56:08 -0700

    move distanceXY and factorial to dot/js/util/, https://github.com/phetsims/models-of-the-hydrogen-atom/issues/118

---
+++ js/util/distanceXY.ts
@@ -0,0 +1,12 @@
+// Copyright 2025, University of Colorado Boulder
+
+/**
+ * Returns the distance between 2 points, given by (x,y) coordinates.
+ *
+ * @author Chris Malley (cmalley@pixelzoom.com)
+ */
+export default function distanceXY( x1: number, y1: number, x2: number, y2: number ): number {
+  const dx = x1 - x2;
+  const dy = y1 - y2;
+  return Math.sqrt( dx * dx + dy * dy );
+}
